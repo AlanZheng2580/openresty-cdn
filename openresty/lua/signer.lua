ca73d7b8eddb1e998cb65248cfe17c26e33d107d
AlanZheng2580 <168727160+AlanZheng2580@users.noreply.github.com>
2025-06-06 15:15:55 +0800

    Sync

--- openresty/lua/signer.lua
+++ openresty/lua/signer.lua
@@ -2,17 +2,21 @@ local signer = require "aws_v4_signer"
 local http = require "resty.http"
 
 -- configurations from NGINX
-local schema    = ngx.var.minio_schema
+local schema      = ngx.var.minio_schema
 local host        = ngx.var.minio_host
 local access_key  = ngx.var.access_key
-local secret_key  = ngx.var.secret_key
+-- local secret_key  = ngx.var.secret_key
+local secret_key  = ngx.shared.secrets:get("MINIO_SECRET_KEY")
+if not secret_key then
+    ngx.log(ngx.ERR, "Nosecret key found in shared dict!")
+    return ngx.status(500)
+end
 local bucket      = ngx.var.bucket_name
 local object      = ngx.var.object_key
 
 if not (host and access_key and secret_key and bucket and object) then
-    ngx.status = 500
     ngx.say("Missing required parameters.")
-    return
+    return ngx.status(500)
 end
 
 local sig = signer.build{
@@ -24,23 +28,8 @@ local sig = signer.build{
     object = object,
 }
 
--- send to MinIO
-local httpc = http.new()
-local res, err = httpc:request_uri(sig.bucket_url, {
-    method = "GET",
-    headers = {
-        ["Host"] = sig.host,
-        ["x-amz-date"] = sig.amz_date,
-        ["x-amz-content-sha256"] = sig.payload_hash,
-        ["Authorization"] = sig.authorization
-    }
-})
-
-if not res then
-    ngx.status = 502
-    ngx.say("Failed to request MinIO: ", err)
-    return
-end
-
-ngx.status = res.status
-ngx.print(res.body)
+-- set headers
+ngx.req.set_header("Host", sig.host)
+ngx.req.set_header("x-amz-date", sig.amz_date)
+ngx.req.set_header("x-amz-content-sha256", sig.payload_hash)
+ngx.req.set_header("Authorization", sig.authorization)
