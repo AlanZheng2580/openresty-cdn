5e86d16c564e1fb53a10269d8f225fe06ba75cd0
AlanZheng2580 <168727160+AlanZheng2580@users.noreply.github.com>
2025-06-25 10:59:11 +0800

    add a variable `ngx.var.secdn_auth_mode` to perform auth based on selected mode

--- openresty/lua/api_key_check.lua
+++ openresty/lua/api_key_check.lua
@@ -1,6 +1,18 @@
 local auth = require("api_key_auth")
+local auth_mode = ngx.var.secdn_auth_mode or "api_key"      -- default to api_key
 local api_key_name = ngx.var.api_key_name
-local ok, err = auth.verify(api_key_name)
+
+local ok, err
+if auth_mode == "api_key" then
+    ok, err = auth.verify(api_key_name)
+elseif auth_mode == "cookie" then
+    ok, err = auth.verify_cookie(api_key_name)
+else
+    ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
+    ngx.say("Unknown auth_mode: " .. tostring(auth_mode))
+    return
+end
+
 if not ok then
     ngx.status = ngx.HTTP_UNAUTHORIZED
     ngx.say(err)
--- openresty/lua/cookie_check.lua
+++
@@ -1,8 +0,0 @@
-local auth = require("api_key_auth")
-local api_key_name = ngx.var.api_key_name
-local ok, err = auth.verify_cookie(api_key_name)
-if not ok then
-    ngx.status = ngx.HTTP_UNAUTHORIZED
-    ngx.say(err)
-    return
-end
