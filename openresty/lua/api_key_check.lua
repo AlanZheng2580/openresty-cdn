8817a6b9386f46b70164cf9709f8b32889249b43
AlanZheng2580 <168727160+AlanZheng2580@users.noreply.github.com>
2025-06-26 12:44:47 +0800

    update

--- openresty/lua/api_key_check.lua
+++ openresty/lua/api_key_check.lua
@@ -4,5 +4,5 @@ local ok, err = auth.verify(api_key_name)
 if not ok then
     ngx.status = ngx.HTTP_UNAUTHORIZED
     ngx.say(err)
-    return
+    return ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
--- openresty/lua/cookie_check.lua
+++ openresty/lua/cookie_check.lua
@@ -4,5 +4,5 @@ local ok, err = auth.verify_cookie(api_key_name)
 if not ok then
     ngx.status = ngx.HTTP_UNAUTHORIZED
     ngx.say(err)
-    return nginx.exit(ngx.HTTP_UNAUTHORIZED)
+    return ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
