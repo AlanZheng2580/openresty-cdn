6006b75d8b32ce7fec5f2c3c4240158aab99bcaf
AlanZheng2580 <168727160+AlanZheng2580@users.noreply.github.com>
2025-09-05 19:15:12 +0800

    Skip verification for CORS preflight request (OPTIONS)

--- openresty/lua/cookie_check.lua
+++ openresty/lua/cookie_check.lua
@@ -1,5 +1,9 @@
 local auth = require("api_key_auth")
 local cors = require("cors")
+local method = ngx.req.get_method()
+if method == "OPTIONS" then
+    return
+end
 local api_key_name = ngx.var.api_key_name
 local ok, err = auth.verify_cookie(api_key_name)
 if not ok then
